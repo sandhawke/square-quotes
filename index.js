05b9cf61da4a640f69fbaa23c62cb24f2535fd6f
Sandro Hawke <sandro@hawke.org>
2019-05-29 11:01:38 -0400

    rewrite to support nested brackets

--- index.js
+++ index.js
@@ -1,59 +1,108 @@
+
+const [balancedPattern, bothPattern] = makeRE()
+
 function convert (s) {
-  const re = rf()
-  s = s.replace(re, quote)
+  s = s.replace(bothPattern, requote)
   return s
 }
 
-function quote (...args) {
+function safeInBrackets (s) {
+  s = s.replace(balancedPattern, '')
+  if (s.match(/[[\]]/)) return false
+  return true
+}
+
+function wrap (s) {
+  if (safeInBrackets(s)) {
+    return '[' + s + ']'
+  }
+  return wrapInQuotes(s)
+}
+
+function wrapInQuotes (s) {
+  s = s.replace(/\\/g, `\\\\`) // replace a \ with a \\ (despite appearances)
+  s = s.replace(/"/g, `\\"`)   // prefix any embedded quotes with \
+  s = s.replace(/\n/g, `\\n`)  // turn any real newlines into \n
+  const result = '"' + s + '"'
+  return result
+}
+
+function requote (text, ...args) {
   let result
   const m = args[args.length - 1]
-  // console.log('quote function %o', m)
+  // console.log('requote function %o', m)
   if (m.dq) {
-    result = m.dq
+    console.assert(text.startsWith('"'))
+    console.assert(text.endsWith('"'))
+    result = text
   } else {
-    let s = m.square
-    s = s.replace(/\\/g, `\\\\`)
-    s = s.replace(/"/g, `\\"`)
-    s = s.replace(/\n/g, `\\n`)
-    result = '"' + s + '"'
+    console.assert(text.startsWith('['))
+    console.assert(text.endsWith(']'))
+    result = wrapInQuotes(text.slice(1, -1))
   }
   // console.log('quote returning', result)
   return result
 }
 
-module.exports = { convert }
+// I believe the cost of raising maxNesting is small and linear, in
+// memory for storing the regexp and initializing it. Let's confirm.
+function makeRE (maxNesting = 10) {
+  
+  // Remember in strings, unlike RE notation, we need to escape
+  // backslashes.  So an RE-escaped leftbracket is written as '\\['
+  // and a literal backslash character in the data will be matched by
+  // \\\\
 
-/*
-
-To edit, copy this down below, then remove whitespace:
-
-function rf () {
-  return /
+  // match a double-quoted string, like "hello" or "Hello, \"World\""
+  // and return value, including the quotes, in group named "dq"
+  const dq = `
     (?<dq>
        "
-       ( [^"\\] | (\\") | \\ )*
+       (?: [^"\\\\] | (?: \\") | \\\\ )*
        "
     )
-    |
-    (
-       \[
-       (?<square>
-          (
-             [^ [ \] ]
-             |
-             (
-               \[
-                   [^ [ \] ]*
-               \]
-             )
-          )*
-       )
-       \]
-    )
-  /g
-}
-*/
+  `
 
-function rf () {
-  return /(?<dq>"([^"\\]|(\\")|\\)*")|(\[(?<square>([^[\]]|(\[[^[\]]*\]))*)\])/g
+  const nest = []
+
+  // match any bracket-free text, like: hello.
+  nest[0] = `
+    (?:
+          [^ [ \\] ]*
+    )
+  `
+
+  // build up nested versions, like
+  // nest[1]: Hello, [World],
+  // nest[2]: [Hello, [World]]
+  // nest[5]: [[[[[a]]]]]
+  for (let i = 1; i <= maxNesting; i++) {
+    nest[i] = `
+      (?: 
+        \\[
+          (?:
+             ${nest[0]}
+          |
+             ${nest[i-1]}
+          )*
+        \\]
+      )
+    `
+  }
+
+  const or = '\n|\n'
+  const balanced = nest.slice(1).join(or)
+  const both = dq + or + balanced
+
+  const strippedBalanced = balanced.replace(/[ \n]/g, '')
+  const strippedBoth = both.replace(/[ \n]/g, '')
+
+  // console.log({strippedBalanced, strippedBoth})
+  // console.log(JSON.stringify({strippedBalanced, strippedBoth}))
+  
+  return [new RegExp(strippedBalanced, 'g'),
+          new RegExp(strippedBoth, 'g')]
 }
+
+module.exports = { convert, safeInBrackets, wrap, makeRE }
+
--- test.js
+++ test.js
@@ -1,5 +1,69 @@
-const { convert } = require('.')
+const sq = require('.')
 const test = require('tape')
+
+const convert = sq.convert
+
+test('re both', t => {
+  const [bal, both] = sq.makeRE(1)
+
+  // console.log({bal, both})
+
+  t.equal('x'.match(both), null)
+  t.assert('""'.match(both))
+  t.assert('x""x'.match(both))
+  t.assert('x"x"x'.match(both))
+  t.assert('[]'.match(both))
+  t.assert('[x]'.match(both))
+  t.assert('x[x]x'.match(both))
+
+  t.end()
+})
+
+test('re bal', t => {
+  let bal = sq.makeRE(2)[0]
+
+  //t.assert('""'.match(bal))
+  t.assert('[]'.match(bal))
+  // t.assert('x'.match(bal))
+  t.assert('x[]'.match(bal))
+  t.assert('a[b]c'.match(bal))
+  t.assert('a[b[c]d]e'.match(bal))
+  t.assert('a[b"[c]d]e'.match(bal))
+
+  t.equal('['.replace(bal, 'X'), '[')
+  t.equal(']'.replace(bal, 'X'), ']')
+  t.equal('[]'.replace(bal, 'X'), 'X')
+  
+  t.end()
+})
+
+test('re nesting', t => {
+  let bal
+
+  bal = sq.makeRE(1)[0]
+  t.equal('[x]'.replace(bal, 'X'), 'X')
+  t.equal('[[x]]'.replace(bal, 'X'), '[X]') // beyond makeRE(1)
+
+  bal = sq.makeRE(2)[0]
+  t.equal('[x]'.replace(bal, 'X'), 'X')
+  t.equal('[[x]]'.replace(bal, 'X'), 'X') 
+  t.equal('[[[x]]]'.replace(bal, 'X'),  '[X]')  // beyond makeRE(2)
+
+  bal = sq.makeRE(20)[0]
+  // 20 of each
+  t.equal('[[[[[[[[[[[[[[[[[[[[x]]]]]]]]]]]]]]]]]]]]'.replace(bal, 'X'), 'X')
+  // 21
+  t.equal('[[[[[[[[[[[[[[[[[[[[[x]]]]]]]]]]]]]]]]]]]]]'.replace(bal, 'X'), '[X]')
+
+  // performance: will anything blow up?
+  bal = sq.makeRE(100)[0]
+  t.equal('[[[[[[[[[[[[[[[[[[[[x]]]]]]]]]]]]]]]]]]]]'.replace(bal, 'X'), 'X')
+
+  // if you get up around 500 you might get errors like Regular
+  // exression too large, after some seconds of churning.
+  
+  t.end()
+})
 
 test('basic', t => {
   t.equal(convert(`[]`), `""`)
@@ -28,22 +92,30 @@ test('avoid content in quotes', t => {
   t.end()
 })
 
-test('nesting one level', t => {
+test('nesting', t => {
   t.equal(convert(`[[]]`), `"[]"`)
   t.equal(convert(`hello [a[b]c] bye`), `hello "a[b]c" bye`)
   t.equal(convert(`hello [a]b] bye`), `hello "a"b] bye`)
   t.equal(convert(`hello [a[b] bye`), `hello [a"b" bye`)
 
+  t.equal(convert(`[[[]]]`), `"[[]]"`)
+  t.equal(convert(`[a[[]]]`), `"a[[]]"`)
+  t.equal(convert(`[a[[x]]]`), `"a[[x]]"`)
+
   t.end()
 })
 
-// NOT IMPLEMENTED YET.
-//
-// Easy enough to do a fixed number of levels.
-// Impossible to do an arbitrary number with a RegExp
-//
-test.skip('nesting two levels', t => {
-  t.equal(convert(`[[[]]]`), `"[[]]"`)
+test('safe', t => {
+  t.assert(sq.safeInBrackets(''))
+  t.assert(sq.safeInBrackets('a'))
+  t.assert(sq.safeInBrackets('[]'))
+  t.assert(sq.safeInBrackets('""'))
+  t.assert(sq.safeInBrackets('[[[[]]]][[[x]]]y[[z]][a]'))
+  t.assert(!sq.safeInBrackets('['))
+  t.assert(!sq.safeInBrackets(']'))
+  t.assert(!sq.safeInBrackets('a[b'))
+  t.assert(!sq.safeInBrackets('a]b'))
+  t.assert(!sq.safeInBrackets('1[2[3]2]1]0]'))
 
   t.end()
 })
